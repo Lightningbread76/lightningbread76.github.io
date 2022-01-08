2bb38cac7701ab47f872cca911d842421eec6173
Kevin Guo <67837759+Lightningbread76@users.noreply.github.com>
2022-01-08 18:21:06 -0500

    Updates

---
+++ precache-manifest.0f9d39207eb3b96d95c38d9c6f5e5101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c37f15cd1679159b469139a9b4a41f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05a5d32b19374099186c",
+    "url": "/static/css/main.d3549954.chunk.css"
+  },
+  {
+    "revision": "87b374d29fdf5c601db5",
+    "url": "/static/js/2.7079edb4.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.7079edb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a5d32b19374099186c",
+    "url": "/static/js/main.4b920a2b.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);

c43e2efe1d71d61985bea003b1d57cd2e93c6f35
Lightningbread76 <67837759+Lightningbread76@users.noreply.github.com>
2025-09-30 15:36:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f175fdf5fe64174c18a2e379e1d2982.js"
+  "/precache-manifest.dadfaf3b0a9b83cba60c714981226add.js"
 );
 
 self.addEventListener('message', (event) => {
